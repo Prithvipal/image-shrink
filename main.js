546a848c0aa936d24fd6400ed8182a4782762fa0
Prithvipal Singh <prithvirathore99@gmail.com>
2020-10-17 17:17:13 +0530

    about menu

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindown() {
   mainWindow = new BrowserWindow({
@@ -20,6 +21,19 @@ function createMainWindown() {
   mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
+function createAboutWindown() {
+  aboutWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: "white",
+  });
+
+  aboutWindow.loadFile(`${__dirname}/app/about.html`);
+}
+
 app.on("ready", () => {
   createMainWindown();
 
@@ -30,10 +44,35 @@ app.on("ready", () => {
 });
 
 const menu = [
-  ...(isMac ? [{ role: "appMenu" }] : []),
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindown,
+            },
+          ],
+        },
+      ]
+    : []),
   {
     role: "fileMenu",
   },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindown,
+            },
+          ],
+        },
+      ]
+    : []),
   ...(isDev
     ? [
         {
