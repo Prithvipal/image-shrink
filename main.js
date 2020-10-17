ab5d5550041215a2db09327a5bab3018da04628c
Prithvipal Singh <prithvirathore99@gmail.com>
2020-10-17 16:47:35 +0530

    Menu roles

--- main.js
+++ main.js
@@ -1,61 +1,66 @@
-const {app, BrowserWindow, Menu, globalShortcut} = require('electron')
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = "develoment";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform === 'darwin' ? true: false
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
-let mainWindow
+let mainWindow;
 
-function createMainWindown(){
-     mainWindow = new BrowserWindow({
-        title: 'ImageShrink',
-        width: 500,
-        height: 600,
-        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-        resizable: isDev,
-        backgroundColor: 'white'
-    })
+function createMainWindown() {
+  mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    backgroundColor: "white",
+  });
 
-    mainWindow.loadFile(`${__dirname}/app/index.html`)
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
-app.on('ready', ()=>{
-  createMainWindown()
+app.on("ready", () => {
+  createMainWindown();
 
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-  globalShortcut.register(isMac ? 'Command+Alt+I': 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
-  mainWindow.on('close', ()=> mainWindow = null)
-})
+  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () =>
+    mainWindow.toggleDevTools()
+  );
+  mainWindow.on("close", () => (mainWindow = null));
+});
 
 const menu = [
-  ...(isMac ? [{role: 'appMenu'}]: [] ),
+  ...(isMac ? [{ role: "appMenu" }] : []),
   {
-    label: 'File',
-    submenu: [
-      {
-        label: "Quit",
-        //accelerator: isMac ? 'Command+W' : 'Ctrl+W', // This also works
-        accelerator: 'CmdOrCtrl+W',
-        click: () => app.quit() 
-      }
-    ]
+    role: "fileMenu",
+  },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-]
+});
 
-
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-  
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createMainWindow()
-    }
-  })
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
